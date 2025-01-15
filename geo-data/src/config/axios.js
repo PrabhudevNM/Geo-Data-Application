cd7144c9b6b57ddd22341f02d12efde4fd95597d
Prabhudev Muttagi <162182231+PrabhudevNM@users.noreply.github.com>
2025-01-16 00:01:25 +0530

    Update axios.js

--- geo-data/src/config/axios.js
+++ geo-data/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://geo-data-application-3j88.onrender.com'
+  baseURL: 'https://geo-data-application-cj6q.onrender.com'
 });
 
 export default axiosInstance;
